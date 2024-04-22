5c11a68dc461356ddf146ce3161708f2409c74d9
Felipe Martinez <fimartinez2@uc.cl>
2024-04-22 18:47:10 -0400

    fix: build error

--- src/components/Inputs/NumberInput.tsx
+++ src/components/Inputs/NumberInput.tsx
@@ -7,36 +7,36 @@ interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   prefix?: string;
 }
 
-const NumberInput: FC<InputProps> = forwardRef<HTMLInputElement, InputProps>(
-  ({ label, className, labelClassName, prefix, ...props }, ref) => {
-    return (
-      <>
-        {label && (
-          <label
-            className={`text-sm text-gray-500 ${labelClassName}`}
-            htmlFor={props.id}
-          >
-            {label}
-          </label>
-        )}
-        {prefix ? (
-          <span
-            className={` flex disabled:cursor-not-allowed placeholder:text-zinc-400 bg-white disabled:bg-gray-100 text-gray-700 text-sm border shadow disabled:shadow-none rounded-md p-2.5 w-full mt-3 mb-3 ${className}`}
-          >
-            {prefix}
-            <input ref={ref} className="focus:outline-none ml-2" {...props} />
-          </span>
-        ) : (
-          <input
-            ref={ref}
-            type="number"
-            className={` disabled:cursor-not-allowed placeholder:text-zinc-400 disabled:bg-gray-100 text-gray-700 text-sm border shadow disabled:shadow-none focus:outline-none rounded-md p-2.5 w-full mt-3 mb-3  ${className}`}
-            {...props}
-          />
-        )}
-      </>
-    );
-  }
-);
+const NumberInput = ({
+  label,
+  className,
+  labelClassName,
+  prefix,
+  ...props
+}: InputProps) => {
+  return (
+    <>
+      {label && (
+        <label className={`text-sm text-gray-500 ${labelClassName}`}>
+          {label}
+        </label>
+      )}
+      {prefix ? (
+        <span
+          className={` flex disabled:cursor-not-allowed placeholder:text-zinc-400 bg-white disabled:bg-gray-100 text-gray-700 text-sm border shadow disabled:shadow-none rounded-md p-2.5 w-full mt-3 mb-3 ${className}`}
+        >
+          {prefix}
+          <input className="focus:outline-none ml-2" {...props} />
+        </span>
+      ) : (
+        <input
+          type="number"
+          className={` disabled:cursor-not-allowed placeholder:text-zinc-400 disabled:bg-gray-100 text-gray-700 text-sm border shadow disabled:shadow-none focus:outline-none rounded-md p-2.5 w-full mt-3 mb-3  ${className}`}
+          {...props}
+        />
+      )}
+    </>
+  );
+};
 
 export default NumberInput;
